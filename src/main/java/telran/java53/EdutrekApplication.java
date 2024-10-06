d495fe6b819c6849ae57180598c107304310fa60
mamontair <mamontair@localhost>
2024-10-06 22:09:39 +0300

    update

--- src/main/java/telran/java53/EdutrekApplication.java
+++ src/main/java/telran/java53/EdutrekApplication.java
@@ -2,7 +2,7 @@ package telran.java53;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test
+
 @SpringBootApplication
 public class EdutrekApplication {
 
