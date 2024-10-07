b267350d2f698c394d7a88117a292f077427fe6d
Evgeniy Tkachenko <tkachenko.evgeniy@gmail.com>
2024-10-07 21:54:56 +0300

    commit-1

---
+++ src/main/java/telran/spring/EduTrecApplication.java
@@ -0,0 +1,13 @@
+package telran.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTrecApplication.class, args);
+	}
+
+}
